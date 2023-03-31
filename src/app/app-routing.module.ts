ddd4d6ef0be0c0f1d1f8d1d7a29ccfd8da7d6a45
Crystal Campbell <blankpage@gmail.com>
2023-03-30 20:21:03 -0500

    chore: add routes for paints, palette, & posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PaintsComponent } from './paints/paints.component';
+import { PaletteComponent } from './palette/palette.component';
+import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
 
@@ -8,7 +11,18 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
-
+  {
+    path: 'palette',
+    component: PaletteComponent
+  },
+  {
+    path: 'paints',
+    component: PaintsComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
 
 ];
 
