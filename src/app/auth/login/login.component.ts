d32639fdf1bfa3af6504f79ac735d3f4b6b5f4fd
Crystal Campbell <blankpage@gmail.com>
2023-04-03 18:27:29 -0500

    feat: login works

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  constructor(private authService:AuthService) { }
+
+  onSubmit(){
+    const userLogin = this.loginForm.value
+
+   this.authService.login(userLogin).subscribe((res: any) => {
+      console.log(res)
+    })
+  }
+
 
   ngOnInit(): void {
   }
